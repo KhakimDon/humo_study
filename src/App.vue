ba4e4cfa319b5db9f918543cfa79593a056fedf3
khakimdon <almamedovkh@mail.ru>
2024-03-16 02:18:03 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import './style.css'
+</script>
+
+<template>
+  
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
